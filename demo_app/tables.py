ef7d37fe149727d23a698c0c2df0a8cacde62bd5
hal <hvong@legalzoom.com>
2019-11-30 21:19:16 -0800

    runs good. needs touchup.

--- demo_app/tables.py
+++ demo_app/tables.py
@@ -1,9 +1,7 @@
 import django_tables2 as tables
-from django.urls import reverse
-import itertools
 from .models import RevenueRecord
 from django.utils.safestring import mark_safe
-from django.conf.urls import url
+from django.utils.text import capfirst
 
 class NumberColumn(tables.Column):
     def render(self, value):
@@ -13,15 +11,14 @@ class DateColumn(tables.Column):
     def render(self, value):
         return mark_safe('<a href="{0}">{0}</a>'.format(value))
 
+class DatePublisherColumn(tables.Column):
+    def render(self, value, record):
+        return mark_safe('<a href="{0}/{1}">{0}</a>'.format(value, record['publisher']))
+
 class RevenueTable(tables.Table):
-    #date = tables.DateTimeColumn(attrs={"th": {"style": "background-color:lightgray; text-transform: capitalize"}})
     date = DateColumn(attrs={"th": {"style": "background-color:lightgray"}})
-    #date = tables.LinkColumn('demo:second', args=[], attrs={"th": {"style": "background-color:lightgray"}})
-    #date = tables.Column(attrs={"th": {"style": "background-color:lightgray; text-transform: capitalize"}})
     revenue_sum = NumberColumn(attrs={"th": {"style": "background-color:lightgray; text-transform: capitalize"}})
     clicks_sum = tables.Column(attrs={"th": {"style": "background-color:lightgray; text-transform: capitalize"}})
-    #publisher = tables.Column(attrs={"th": {"style": "background-color:lightgray"}})
-    #source = tables.Column(attrs={"th": {"style": "background-color:lightgray"}})
 
     class Meta:
         model =  RevenueRecord
@@ -29,10 +26,33 @@ class RevenueTable(tables.Table):
         attrs = {'class': "table table-striped"}
         order_by = '-date'
         fields = ("date","revenue_sum","clicks_sum")
-        #fields = ("date","publisher__name","source__name","revenue_sum","clicks_sum")
 
-    #idx = tables.Column(empty_values=(), orderable=False)
-    #def render_idx(self):
-    #    self.row_counter = getattr(self, 'row_counter', itertools.count(1))
-    #    return next(self.row_counter)
+class DateTable(tables.Table):
+    date = DatePublisherColumn(attrs={"th": {"style": "background-color:lightgray"}})
+    publisher__name = tables.Column(attrs={"th": {"style": "background-color:lightgray; text-transform: capitalize"},
+                                           "td": {"style": "text-transform: capitalize"},
+                                           })
+    revenue_sum = NumberColumn(attrs={"th": {"style": "background-color:lightgray; text-transform: capitalize"}})
+    clicks_sum = tables.Column(attrs={"th": {"style": "background-color:lightgray; text-transform: capitalize"}})
+
+    class Meta:
+        model =  RevenueRecord
+        template_name = "django_tables2/bootstrap.html"
+        attrs = {'class': "table table-striped"}
+        fields = ("date","publisher__name","revenue_sum","clicks_sum")
+        order_by = 'publisher__name'
+
+class PublisherTable(tables.Table):
+    source__name = tables.Column(attrs={"th": {"style": "background-color:lightgray; text-transform: capitalize"},
+                                           "td": {"style": "text-transform: capitalize"},
+                                       })
+    revenue_sum = NumberColumn(attrs={"th": {"style": "background-color:lightgray; text-transform: capitalize"}})
+    clicks_sum = tables.Column(attrs={"th": {"style": "background-color:lightgray; text-transform: capitalize"}})
+
+    class Meta:
+        model =  RevenueRecord
+        template_name = "django_tables2/bootstrap.html"
+        attrs = {'class': "table table-striped"}
+        fields = ("source__name","revenue_sum","clicks_sum")
+        order_by = 'source__name'
 
--- demo_app/views.py
+++ demo_app/views.py
@@ -3,13 +3,12 @@ from django.db.models import Sum
 from django.views import View
 from django_tables2 import SingleTableView, RequestConfig
 from .models import RevenueRecord, Publisher, Source
-from .tables import RevenueTable
+from .tables import RevenueTable, DateTable, PublisherTable
 from django.shortcuts import render, get_object_or_404
 import datetime
 
 class IndexView(View):
     def get(self, request):
-        #table = RevenueTable(RevenueRecord.objects.values('date').annotate(revenue_sum=Sum('revenue'), clicks_sum=Sum('clicks')).order_by('-date'))
         table = RevenueTable(RevenueRecord.objects.values('date').annotate(revenue_sum=Sum('revenue'), clicks_sum=Sum('clicks')).order_by('-date'))
         RequestConfig(request).configure(table)
         table.paginate(page=request.GET.get("page", 1), per_page=15)
@@ -18,14 +17,14 @@ class IndexView(View):
 class DateView(View):
     def get(self, request, q_date):
         ddate = datetime.datetime.strptime(q_date,"%Y-%m-%d").strftime("%B %d, %Y")
-        records = RevenueRecord.objects.filter(date=q_date).values('date', 'publisher','publisher__name').annotate(revenue_sum=Sum('revenue'), clicks_sum=Sum('clicks')).order_by('publisher__name')
-        return render(request, 'date_template.html', {'publishers': records, 'q_date': ddate})
+        table = DateTable(RevenueRecord.objects.filter(date=q_date).values('date', 'publisher','publisher__name').annotate(revenue_sum=Sum('revenue'), clicks_sum=Sum('clicks')).order_by('publisher__name'))
+
+        return render(request, 'date_template.html', {'table': table, 'q_date': ddate})
 
 class PublisherView(SingleTableView):
     def get(self, request, q_date, q_publisher):
-
         publisher = get_object_or_404(Publisher, id=q_publisher)
         ddate = datetime.datetime.strptime(q_date,"%Y-%m-%d").strftime("%B %d, %Y")
+        table = PublisherTable(RevenueRecord.objects.filter(date=q_date,publisher=publisher).values("source__name").annotate(revenue_sum=Sum('revenue'), clicks_sum=Sum('clicks')).order_by('source__name'))
 
-        table = RevenueTable(RevenueRecord.objects.filter(date=q_date,publisher=q_publisher).values("source__name").annotate(revenue_sum=Sum('revenue'), clicks_sum=Sum('clicks')).order_by('source__name'))
         return render(request, 'publisher_template.html', {'table': table})
